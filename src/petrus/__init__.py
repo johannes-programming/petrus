a62d21e4b84e7b726c9ce09e3eec9f8943e824a4
Johannes <johannes-programming@mailfence.com>
2024-08-22 16:05:58 +0200

    Version 0.0.7

--- src/petrus/__init__.py
+++ src/petrus/__init__.py
@@ -1,5 +1,5 @@
 from petrus._progs import main
-    
+
 if __name__ == '__main__':
     main() 
 
--- src/petrus/_progs.py
+++ src/petrus/_progs.py
@@ -1,12 +1,13 @@
 
-from argparse import ArgumentParser
-import subprocess
 import os
 import shutil
+import subprocess
 import sys
+from argparse import ArgumentParser
 
-import tomllib
 import tomli_w
+import tomllib
+
 
 def main(args=None):
     parser = ArgumentParser()
@@ -18,30 +19,39 @@ def main(args=None):
 
 def run(root=None, *, version=None):
     run_root(root)
-    run_version_pyproject(version=version)
+    run_isort()
+    run_pyproject(version=version)
     run_commit(version=version)
     run_end()
+
+def run_isort():
+    try:
+        subprocess.run(['isort', '.'], check=True)
+    except subprocess.CalledProcessError:
+        pass
 
 def run_root(root):
     if root is None:
         return
     os.chdir(root)
 
-def run_version_pyproject(version):
-    if version is None:
-        return
+def run_pyproject(version):
     if not os.path.isfile("pyproject.toml"):
         return
     with open("pyproject.toml", "rb") as f:
         pyproject = tomllib.load(f)
-    pyproject['project']['version'] = version
+    if version is not None:
+        pyproject['project']['version'] = version
+    pyproject['project']['classifiers'].sort()
+    pyproject['project']['dependencies'].sort()
     with open("pyproject.toml", "wb") as f:
         tomli_w.dump(pyproject, f)
 
 def run_commit(version):
     if version is None:
-        return
-    message = f"Version {version}"
+        message = "a"
+    else:
+        message = f"Version {version}"
     try:
         subprocess.run(['git', 'add', '-A'], check=True)
         subprocess.run(['git', 'commit', '--allow-empty', '-m', message], check=True)
@@ -55,6 +65,4 @@ def run_end():
         subprocess.run(["twine", "upload", "dist/*"], check=True)
     except:
         return
-
-
 
