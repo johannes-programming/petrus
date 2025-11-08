d39511df0033ee1c55b286ca75db77203ac20839
Johannes <johannes.programming@gmail.com>
2025-11-08 17:58:43 +0100

    Version 0.7.0

--- src/petrus/_core/calcs/Calc.py
+++ src/petrus/_core/calcs/Calc.py
@@ -1,43 +1,27 @@
 from typing import *
 
+from .BaseCalc import BaseCalc
 
-class Calc:
+
+class Calc(BaseCalc):
     _CORE = "prog"
 
-    def __delattr__(self: Self, name):
-        self.__check(name)
-        object.__delattr__(self, name)
-
-    def __getattr__(self: Self, name):
-        name = str(name)
-        if name.startswith("_"):
-            raise AttributeError(name)
+    def __getattr__(self: Self, name: Any) -> Any:
+        name_: str
+        name_ = str(name)
+        if name_.startswith("_"):
+            raise AttributeError(name_)
         if not hasattr(self, "_lock"):
             self._lock = set()
-        if name in self._lock:
+        if name_ in self._lock:
             raise Exception
-        self._lock.add(name)
+        self._lock.add(name_)
         try:
-            ans = self._calc(name)
-            object.__setattr__(self, name, ans)
+            ans = self._calc(name_)
+            object.__setattr__(self, name_, ans)
         finally:
-            self._lock.remove(name)
+            self._lock.remove(name_)
         return ans
-
-    def __init__(self: Self, core, /) -> None:
-        object.__setattr__(self, type(self)._CORE, core)
-        getattr(self, "__post_init__", int)()
-
-    def __setattr__(self: Self, name, value):
-        self.__check(name)
-        object.__setattr__(self, name, value)
-
-    def __check(self: Self, name):
-        if name.startswith("_"):
-            return
-        if not hasattr(super(), name):
-            return
-        raise AttributeError("readonly")
 
     def _calc(self: Self, name):
         return getattr(self, f"_calc_{name}")()
