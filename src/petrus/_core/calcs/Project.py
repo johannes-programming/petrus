4be7ea51ab25c22c7eac2ba5bff41b8a5a7d5ab9
Johannes <johannes.programming@gmail.com>
2025-11-09 08:16:10 +0100

    Version 0.9.26

--- src/petrus/_core/calcs/Project.py
+++ src/petrus/_core/calcs/Project.py
@@ -179,6 +179,29 @@ class Project(Calc):
         ans = self.prog.easy_dict(ans)
         return ans
 
+    def __getattr__(self: Self, name: Any) -> Any:
+        ans: Any
+        name_: str
+        name_ = str(name)
+        if name_ != "version":
+            return object.__getattribute__(self, name_)
+        if name_.startswith("_"):
+            raise AttributeError(name_)
+        if not hasattr(self, "_lock"):
+            self._lock = set()
+        if name_ in self._lock:
+            raise Exception
+        self._lock.add(name_)
+        try:
+            ans = self._calc(name_)
+            object.__setattr__(self, name_, ans)
+        finally:
+            self._lock.remove(name_)
+        return ans
+
+    def _calc(self: Self, name: Any) -> Any:
+        return getattr(self, f"_calc_{name}")()
+
     def _calc_version(self: Self) -> Any:
         return self.prog.version_formatted
 
