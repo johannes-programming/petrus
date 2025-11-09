670067e7d6fd4381eaef6ddcea318968676e483f
Johannes <johannes.programming@gmail.com>
2025-11-09 09:03:11 +0100

    Version 0.9.43

--- src/petrus/_core/calcs/Draft.py
+++ src/petrus/_core/calcs/Draft.py
@@ -1,9 +1,19 @@
 import importlib.resources
 from typing import *
 
-from petrus._core.calcs.Calc import Calc
+from petrus._core.calcs.BaseCalc import BaseCalc
 
 
-class Draft(Calc):
-    def _calc(self: Self, name: Any) -> str:
-        return importlib.resources.read_text("petrus.drafts", "%s.txt" % name)
+class Draft(BaseCalc):
+    def __post_init__(self: Self) -> None:
+        self._data = dict()
+
+    def __getattr__(self: Self, name: str) -> Any:
+        return self[name]
+
+    def __getitem__(self: Self, key: str) -> str:
+        if key not in self._data.keys():
+            self._data[key] = importlib.resources.read_text(
+                "petrus.drafts", "%s.txt" % key
+            )
+        return self._data[key]
