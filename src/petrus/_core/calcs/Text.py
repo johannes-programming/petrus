9c52c5efa38597082f0ce6d7127082f72de7f552
Johannes <johannes.programming@gmail.com>
2025-11-09 15:59:46 +0100

    Version 0.9.55

--- src/petrus/_core/calcs/Text.py
+++ src/petrus/_core/calcs/Text.py
@@ -38,35 +38,35 @@ class Text(BaseCalc):
             return lines
         try:
             f = getattr(self, "_calc_" + name)
-        except:
+        except Exception:
             return ""
         return f()
 
     def _calc_core(self: Self) -> Any:
         n = self.prog.project.name
-        return self.prog.draft.core.format(project=n)
+        return self.prog.draft.getitem("core").format(project=n)
 
     def _calc_gitignore(self: Self) -> Any:
         return self.prog.draft.getitem("gitignore")
 
     def _calc_init(self: Self) -> Any:
         n = self.prog.project.name
-        return self.prog.draft.init.format(project=n)
+        return self.prog.draft.getitem("init").format(project=n)
 
     def _calc_license(self: Self) -> Any:
         d = dict()
         d["year"] = self.prog.year
         d["author"] = self.prog.author[0]
-        ans = self.prog.draft.license.format(**d)
+        ans = self.prog.draft.getitem("license").format(**d)
         return ans
 
     def _calc_main(self: Self) -> Any:
         n = self.prog.project.name
-        return self.prog.draft.main.format(project=n)
+        return self.prog.draft.getitem("main").format(project=n)
 
     def _calc_manifest(self: Self) -> Any:
         n = self.prog.project.name
-        return self.prog.draft.manifest.format(project=n)
+        return self.prog.draft.getitem("manifest").format(project=n)
 
     def _calc_readme(self: Self) -> Any:
         return self.prog.block.text
