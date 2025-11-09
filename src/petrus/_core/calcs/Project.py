3051e3e3d9f76a1ffd2dfcb8e01eff07511d5ba4
Johannes <johannes.programming@gmail.com>
2025-11-09 08:48:25 +0100

    Version 0.9.36

--- src/petrus/_core/calcs/Project.py
+++ src/petrus/_core/calcs/Project.py
@@ -17,7 +17,6 @@ class _empty:
 class Project(BaseCalc):
     def __post_init__(self: Self) -> None:
         self._version = _empty
-        self._lock = set()
 
     @cached_property
     def authors(self: Self) -> Any:
@@ -187,12 +186,11 @@ class Project(BaseCalc):
 
     @property
     def version(self: Self) -> Any:
-        if self._version is not _empty:
-            return self._version
-        self._version = self._calc_version()
+        if self._version is _empty:
+            self._version = self.__version()
         return self._version
 
-    def _calc_version(self: Self) -> Any:
+    def __version(self: Self) -> Any:
         return self.prog.version_formatted
 
     @classmethod
