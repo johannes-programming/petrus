905ff39d7f3ace1cb208b518193d49ff7b05a16d
Johannes <johannes.programming@gmail.com>
2025-11-09 09:06:10 +0100

    Version 0.9.45

--- src/petrus/_core/calcs/Text.py
+++ src/petrus/_core/calcs/Text.py
@@ -1,9 +1,29 @@
 from typing import *
 
-from petrus._core.calcs.Calc import Calc
+from .BaseCalc import BaseCalc
 
 
-class Text(Calc):
+class Text(BaseCalc):
+    def __getattr__(self: Self, name: Any) -> Any:
+        ans: Any
+        name_: str
+        name_ = str(name)
+        if hasattr(type(self), name_):
+            return object.__getattribute__(self, name_)
+        if name_.startswith("_"):
+            raise AttributeError(name_)
+        if not hasattr(self, "_lock"):
+            self._lock = set()
+        if name_ in self._lock:
+            raise Exception
+        self._lock.add(name_)
+        try:
+            ans = self._calc(name_)
+            object.__setattr__(self, name_, ans)
+        finally:
+            self._lock.remove(name_)
+        return ans
+
     def _calc(self: Self, name: Any) -> Any:
         f = getattr(self.prog.file, name)
         try:
