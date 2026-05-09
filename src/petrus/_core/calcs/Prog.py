87ed5e83b67e43ce941dcee21fcb2fe1467443ec
Johannes <johannes-programming@mailfence.com>
2026-05-09 22:02:47 +0200

    Version 0.0.11

--- src/petrus/_core/calcs/Prog.py
+++ src/petrus/_core/calcs/Prog.py
@@ -14,13 +14,13 @@ from petrus._core.TOML import TOML
 class Prog(Calc):
     _CORE = "kwargs"
     INPUTS = {
-        'description':None,
-        'author':None,
-        'email':None, 
-        'requires_python':None, 
-        'github':None, 
-        'project_version':None,
-        'year':None,
+        "description": None,
+        "author": None,
+        "email": None,
+        "requires_python": None,
+        "github": None,
+        "project_version": None,
+        "year": None,
     }
 
     def __post_init__(self):
@@ -37,9 +37,11 @@ class Prog(Calc):
         self.save("setup")
         self.packages
         utils.run_isort()
+        utils.run_black(os.getcwd())
         self.git.commit_version()
         self.git.push()
         utils.pypi()
+
     def _calc_build_system(self):
         ans = self.pp["build-system"]
         if type(ans) is dict:
@@ -50,17 +52,22 @@ class Prog(Calc):
         ans["requires"] = ["setuptools>=61.0.0"]
         ans["build-backend"] = "setuptools.build_meta"
         return ans
+
     def _calc_draft(self):
         return Draft(self)
+
     def _calc_file(self):
         return File(self)
+
     def _calc_git(self):
         return Git(self)
+
     def _calc_github(self):
         u = self.kwargs["github"]
         if u == "":
             return ""
         return f"https://github.com/{u}/{self.project.name}"
+
     def _calc_packages(self):
         utils.mkdir("src")
         ans = []
@@ -77,22 +84,26 @@ class Prog(Calc):
             self.save("init")
             self.save("main")
         return ans
+
     def _calc_pp(self):
         return TOML(text=self.text.pp)
+
     def _calc_project(self):
         return Project(self)
+
     def _calc_text(self):
         return Text(self)
+
     def _calc_year(self):
-        return (
-            self.kwargs["year"] 
-            or str(datetime.datetime.now().year))
+        return self.kwargs["year"] or str(datetime.datetime.now().year)
+
     @staticmethod
     def _is_pkg(path):
         if not os.path.isdir(path):
             return False
         f = os.path.join(path, "__init__.py")
         return utils.isfile(f)
+
     def save(self, n, /):
         file = getattr(self.file, n)
         text = getattr(self.text, n)
@@ -101,6 +112,3 @@ class Prog(Calc):
             os.mkdir(root)
         with open(file, "w") as s:
             s.write(text)
-
-        
-
