ac4afbe377a22e0b4be8b6e9d63a56bb5783436c
Johannes <johannes.programming@gmail.com>
2026-05-09 23:42:29 +0200

    Version 0.10.47

--- src/petrus/_core/utils.py
+++ src/petrus/_core/utils.py
@@ -12,7 +12,7 @@ import isort
 import requests
 
 TEXT_EXTS: tuple[str, ...]
-TEXT_EXTS = (".cfg", ".css", ".html", ".in", ".js", ".rst", ".toml", ".txt")
+TEXT_EXTS = (".cfg", ".css", ".html", ".in", ".js", ".rst", ".toml", ".txt", ".typed")
 
 
 def dict_match(a: Any, b: Any, /) -> bool:
